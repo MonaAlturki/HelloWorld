f92b76034638d8456924f7ebd6da8529abe7382c
MonaAlturki <mona.alturki11@gmail.com>
2019-04-01 05:23:06 +0300

    frist commit

---
+++ Contact.php
@@ -0,0 +1,140 @@
+<?php require ("header.php"); ?>
+  <div class="clearfix"></div>
+    <div class="container" style="margin-top:20px;">
+	  <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">العمادة</p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">0165397115</p>
+                </div>
+
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:a.alkalel@uoh.edu.sa">a.alkalel@uoh.edu.sa </a></p>
+                </div>
+
+            </div>
+        </div>
+		 <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">مدير مكتب العميد:</p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">0165397116</p>
+                </div>
+
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:#">...... </a></p>
+                </div>
+
+            </div>
+        </div>
+		<div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">مدير ادارة الجودة والاعتماد :</p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">0165397226</p>
+                </div>
+
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:#">...... </a></p>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">QA CCSE Department Co-ordinator</p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">016000000</p>
+                </div>
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:shumyla.uoh@gmail.com">Shumyla Rasheed </a></p>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">CS program </p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">016000000</p>
+                </div>
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:z.malikuoh@gmail.com">Zahida sarwar </a></p>
+                </div>
+
+            </div>
+        </div>
+        <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">COE program  </p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">016000000</p>
+                </div>
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:Sondess.benaoun@gmail.com">Sondess </a></p>
+                </div>
+
+            </div>
+        </div>
+       <div class="col-md-12" style="height:10px;"></div>
+        <div class="col-md-4 contact-info">
+            <div class="w3ls-cont">
+                <div class="address">
+                    <i class="glyphicon glyphicon-ok"></i>
+                    <p class="p3">SWE program   </p>
+
+                </div>
+                <div class="phone">
+                    <i class="glyphicon glyphicon-phone-alt"></i>
+                    <p class="p4">016000000</p>
+                </div>
+                <div class="email-info">
+                    <i class="glyphicon glyphicon-envelope"></i>
+                    <p class="p4"><a href="mailto:vijipriyajkv@gmail.com">Vijipriya </a></p>
+                </div>
+
+            </div>
+        </div>
+       
+       
+
+       
+        <div class="clearfix"></div>
+    </div>
+    <?php require ("foot.php"); ?>
---
+++ foot.php
@@ -0,0 +1,32 @@
+﻿<!-- services -->
+<div class="services">
+    <div class="container">
+
+    </div>
+</div>
+<!-- //services -->
+<!-- foot-agileits -->
+<div class="foot-agileits-w3layouts">
+    <div class="container">
+
+        <div class="clearfix"></div>
+        <div class="copyright-agileinfo">
+            <p>© 2018 Quality Assurance Evaluation System. All Rights Reserved </p>
+        </div>
+        <ul class="social-icons1">
+						<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+						<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					</ul>
+          <br/>
+    </div>
+</div>
+<!-- //foot-agileits -->
+<!-- for bootstrap working -->
+<script src="js/bootstrap.js"></script>
+<!-- //for bootstrap working -->
+<script src="js/responsiveslides.min.js"></script>
+
+</body>
+</html>
